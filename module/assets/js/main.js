ddb75d2e5bddb7799de95996c8b9e9e806311b24
suvradip <suvradip@fusioncharts.com>
2015-12-28 17:58:59 +0530

    changes

--- module/assets/js/main.js
+++ module/assets/js/main.js
@@ -4,18 +4,52 @@
 var PROFEEDER = {};
 
 //storing window height and size
-PROFEEDER.windowHeight = $(window).height();
+PROFEEDER.windowHeight = $(document).height();
 PROFEEDER.windowWidth = $(window).width();
 
 
 //setup div size according to viewport
-
 var headerH = $('.wrapper .header').outerHeight();
 var footerH = $('.wrapper .footer').outerHeight();
-var contenHeight = PROFEEDER.windowWidth - (headerH + footerH +170);
-console.log(PROFEEDER.windowWidth +' '+contenHeight);
+var contenHeight = PROFEEDER.windowHeight - (headerH + footerH + 6);
 $('.wrapper .sidebar').css('height', contenHeight);
 $('.wrapper .container').css('height', contenHeight);
+
+
+
+
+//sidebar item set
+
+$.getJSON( "assets/js/attributes/Angular.json", function( data ) {
+$('.sidebar').append(
+	$('<ul>').append(function(){
+		var para = $(this);
+		$.each(data, function(i, el) {
+			para.append(
+				$('<li>').attr('class', 'items').append(function(){
+					var child = $(this);
+
+					child.append(
+						$('<a>').text(data[i].table));
+
+					child.append(
+						$('<ul>').append(function(){
+						var child1 = $(this);
+						$.each(data[i].attrs, function(j, el) {
+						child1.append(
+							$('<li>').append(
+								$('<a>').text(data[i].attrs[j].name)));	
+						});
+					}));
+				}));			
+		});
+	}));
+
+//sidebar navigation onClick list appear
+$('.items').on('click', function(event) {
+	$(this).children('.items ul').toggle('slow');
+});
+});
 
 })();
 
