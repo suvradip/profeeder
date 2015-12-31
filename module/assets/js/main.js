db932296572dc7629e9e1ed6a79eb3d71169167d
suvradip <suvradip@fusioncharts.com>
2015-12-31 16:37:54 +0530

    changes

--- module/assets/js/main.js
+++ module/assets/js/main.js
@@ -23,22 +23,7 @@ $('.sidebar').append(
 		var para = $(this);
 		$.each(data, function(i, el) {
 			para.append(
-				$('<li>').attr('class', 'items').append(function(){
-					var child = $(this);
-
-					child.append(
-						$('<a>').text(data[i].table));
-
-					// child.append(
-					// 	$('<ul>').append(function(){
-					// 	var gchild = $(this);
-					// 	$.each(data[i].attrs, function(j, el) {
-					// 	gchild.append(
-					// 		$('<li>').append(
-					// 			$('<a>').text(data[i].attrs[j].name)));	
-					// 	});
-					// }));
-				}));			
+				$('<li>').attr('class', 'items').append($('<a>').text(data[i].table)));			
 		});
 	}));
 return data;
@@ -49,7 +34,14 @@ return data;
 //initial render chart
 PROFEEDER.chartData = $.getJSON('assets/js/chart/column2d.json', function(data) {
 	var fc = new FusionCharts(data).render();
-	$('.code-area textarea').text(JSON.stringify(data, null, 4));
+	$('.code-area textarea').val(JSON.stringify(data, null, 4));
+
+	// $.each(PROFEEDER.chartAttributes.responseJSON, function(index, obj) {
+	// 	$.each(obj.attrs, function(index, val) {
+	// 		if(obj.hasOwnProperty)
+	// 	});
+	// });
+
 	return data;
 });
 
@@ -61,7 +53,14 @@ window.setTimeout(function(){
 });
 }, 2000);
 
+//on changes in textArea
+$('.code-area textarea').change(function(event) {
+	FusionCharts.items[Object.keys(FusionCharts.items)].dispose();
+	var fc = new FusionCharts(JSON.parse(event.target.value)).render();
+	PROFEEDER.chartData.responseJSON = JSON.parse(event.target.value);
+});
 
+//attribute set
 PROFEEDER.setAttribute = (function(param){
 	var attr = PROFEEDER.chartData.responseJSON;
 	
@@ -72,12 +71,13 @@ PROFEEDER.setAttribute = (function(param){
 	PROFEEDER.chartData.responseJSON = attr;
 	FusionCharts.items[Object.keys(FusionCharts.items)].dispose();
 
-	$('.code-area textarea').text(JSON.stringify(attr, null, 4));	
-	var fc = new FusionCharts(JSON.parse($('.code-area textarea').text())).render();
+	var textarea = $('.code-area textarea');
+	textarea.val(JSON.stringify(attr, null, 4));	
+	var fc = new FusionCharts(JSON.parse(textarea.val())).render();
 	
 });
 
-
+//elemnet making
 PROFEEDER.element = {
 	select: function(start, end, array){
 				var temp;
@@ -94,7 +94,7 @@ PROFEEDER.element = {
 
 };
 
-
+//element creation and set events
 PROFEEDER.viewAttributes = (function(param){
 	$('.chart-attribute-settings').text('');
 	$.each(PROFEEDER.chartAttributes.responseJSON, function(index_1, val_1) {
@@ -104,43 +104,67 @@ PROFEEDER.viewAttributes = (function(param){
 				$('.chart-attribute-settings').append(
 					$('<p>').append(function(){
 						$(this).attr('style', 'box-sizing: border-box; display:inline-block; *zoom:1; *display:inline; padding:3px 5px; width:33.3%; margin:0;');
-						$(this).append($('<label for="L'+ index_1 + '_' +index_2 +'" title="'+ val_2.description+'">').text(val_2.name));
+						$(this).append($('<label style="display:inline-block; min-width:150px;" for="L'+ index_1 + '_' +index_2 +'" title="'+ val_2.description+'">').text(val_2.name));
+						
 						if(val_2.type === 'Boolean')
 							$(this).append('<input type="checkbox" id="L'+ index_1 + '_' +index_2 +'"/> ');
 						if(val_2.type === 'Color')
 							$(this).append('<input type="color" /> ');
 						if(val_2.type === 'Number')
-							$(this).append('<input type="number" /> ');			
-						if(val_2.type === 'String' && val_2.range !== ''){
-							var range = val_2.range.split(',');
-							$(this).append(PROFEEDER.element.select(0,range.length,range));
+							$(this).append('<input type="number" /> ');	
+
+						if(val_2.type === 'String' &&  typeof (val_2.range) !== 'undefined'){
+							if(val_2.range !== ''){
+								var range = val_2.range.split(',');
+								$(this).append(PROFEEDER.element.select(0,range.length,range));
+							}
+
+							if(val_2.range === '')
+								$(this).append('<input type="text" /> ');
 						}
-						if(val_2.type === 'String' && val_2.range === '')
+
+						if(val_2.type === 'String' &&  typeof (val_2.range) === 'undefined')
 							$(this).append('<input type="text" /> ');
+							
 
 					}));
 			});
 		}	
 	});
 
-		$('input').on('click' , function(event) {
+		//set possible events for each element
+		$('input[type="checkbox"]').on('click' , function(event) {
 			var param ={};
 			param.key = $(this).prev().text();
 	
-			if($(this)[0].type === 'checkbox'){
-				console.log('111');
+			if(event.target.type === 'checkbox'){
+		
 				if($(this).prop('checked'))
 					param.value =1;
 				else
 					param.value =0;	
 			}
 
-			if($(this)[0].type === 'text')
+			if(event.target.type === 'text')
 				param.value = $(this).val();
 			
-			if($(this)[0].type === 'number')
+			if(event.target.type === 'number')
 				param.value = $(this).val();
 
+			PROFEEDER.setAttribute(param);
+		});
+
+		$('input[type="color"], input[type="number"]').change(function(event) {
+			var param = {};
+			param.key = $(this).prev().text();
+			param.value = event.target.value;
+			PROFEEDER.setAttribute(param);
+		});
+
+		$('select').change(function(event) {
+			var param = {};
+			param.key = $(this).prev().text();
+			param.value = event.target.value;
 			PROFEEDER.setAttribute(param);
 		});
 });
