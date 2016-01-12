1c526e070781b5e828163fafa8242310e8b051fd
suvradip <suvradip@fusioncharts.com>
2016-01-12 18:32:15 +0530

    changes

--- module/assets/js/main.js
+++ module/assets/js/main.js
@@ -6,6 +6,10 @@ var PROFEEDER = {};
 //storing window height and size
 PROFEEDER.windowHeight = $(document).height();
 PROFEEDER.windowWidth = $(window).width();
+//chart type
+PROFEEDER.charts = [
+'select', 'area2d', 'bar2d', 'bar3d', 'column2d'
+];
 
 
 //setup div size according to viewport
@@ -15,67 +19,6 @@ var contenHeight = PROFEEDER.windowHeight - (headerH + footerH + 6);
 $('.wrapper .sidebar').css('height', contenHeight);
 $('.wrapper .container').css('height', contenHeight);
 
-
-//sidebar item set
-PROFEEDER.chartAttributes = $.getJSON( "assets/js/attributes/Column2D.json", function( data ) {
-$('.sidebar').append(
-	$('<ul>').append(function(){
-		var para = $(this);
-		$.each(data, function(i, el) {
-			para.append(
-				$('<li>').attr('class', 'items').append($('<a>').text(data[i].table)));			
-		});
-	}));
-return data;
-});
-
-
-
-//initial render chart
-PROFEEDER.chartData = $.getJSON('assets/js/chart/column2d.json', function(data) {
-	var fc = new FusionCharts(data).render();
-	$('.code-area textarea').val(JSON.stringify(data, null, 4));
-
-	// $.each(PROFEEDER.chartAttributes.responseJSON, function(index, obj) {
-	// 	$.each(obj.attrs, function(index, val) {
-	// 		if(obj.hasOwnProperty)
-	// 	});
-	// });
-
-	return data;
-});
-
-//sidebar navigation onClick list appear
-window.setTimeout(function(){
-	$('.items').on('click', function(event) {
-	//$(this).children('ul').toggle('slow');
-	PROFEEDER.viewAttributes($(this).children('a').text());
-});
-}, 2000);
-
-//on changes in textArea
-$('.code-area textarea').change(function(event) {
-	FusionCharts.items[Object.keys(FusionCharts.items)].dispose();
-	var fc = new FusionCharts(JSON.parse(event.target.value)).render();
-	PROFEEDER.chartData.responseJSON = JSON.parse(event.target.value);
-});
-
-//attribute set
-PROFEEDER.setAttribute = (function(param){
-	var attr = PROFEEDER.chartData.responseJSON;
-	
-	if(attr.dataSource.hasOwnProperty('chart'))
-		attr.dataSource.chart[param.key] = param.value;
-	
-	
-	PROFEEDER.chartData.responseJSON = attr;
-	FusionCharts.items[Object.keys(FusionCharts.items)].dispose();
-
-	var textarea = $('.code-area textarea');
-	textarea.val(JSON.stringify(attr, null, 4));	
-	var fc = new FusionCharts(JSON.parse(textarea.val())).render();
-	
-});
 
 //elemnet making
 PROFEEDER.element = {
@@ -91,20 +34,102 @@ PROFEEDER.element = {
 				temp += '</select>';
 				return temp;
 			}
-
 };
+
+
+//chart dropdown list and binding event
+$('.sidebar').append(PROFEEDER.element.select(0,PROFEEDER.charts.length, PROFEEDER.charts)).promise().done(function(){
+	$(this).on('change', function(event) {
+		console.log(event.target.value);
+		PROFEEDER.currentChartType = event.target.value;
+		PROFEEDER.chartAttributes.returnData = PROFEEDER.chartAttributes();
+
+		//sidebar navigation onClick list appear
+		window.setTimeout(function(){
+			$('.items').on('click', function(event) {
+			//$(this).children('ul').toggle('slow');
+			PROFEEDER.viewAttributes($(this).children('a').text());
+			});
+		}, 2000);
+		
+		PROFEEDER.chartData.returnData =PROFEEDER.chartData();
+	});
+});
+
+
+//sidebar attribute list 
+PROFEEDER.chartAttributes = (function (){
+	var data = $.getJSON( 'assets/js/attributes/'+ PROFEEDER.currentChartType +'.json', function( data ) {
+	$('.sidebar').append(
+		$('<ul>').append(function(){
+			var para = $(this);
+			$.each(data, function(i, el) {
+				para.append(
+					$('<li>').attr('class', 'items').append($('<a>').text(data[i].table)));			
+			});
+		}));
+	return data;
+	});	
+
+	return data;
+});
+
+
+// render chart
+PROFEEDER.chartData = (function (){
+		var data = $.getJSON('assets/js/chart/'+ PROFEEDER.currentChartType +'.json', function(data) {
+		var fc = new FusionCharts(data).render();
+		$('.code-area textarea').val(JSON.stringify(data, null, 4));
+		return data;
+	});
+
+	return data;	
+});
+
+
+
+//on changes in textArea
+$('.code-area textarea').change(function(event) {
+	for (var charts in FusionCharts.items)
+        FusionCharts.items[charts].dispose();
+	var fc = new FusionCharts(JSON.parse(event.target.value)).render();
+	PROFEEDER.chartData.returnData.responseJSON = JSON.parse(event.target.value);
+});
+
+//attribute set
+PROFEEDER.setAttribute = (function(param){
+	var attr = PROFEEDER.chartData.returnData.responseJSON;
+	
+	if(attr.dataSource.hasOwnProperty('chart'))
+		attr.dataSource.chart[param.key] = param.value;
+	
+	
+	PROFEEDER.chartData.returnData.responseJSON = attr;
+	for (var charts in FusionCharts.items)
+        FusionCharts.items[charts].dispose();
+            
+
+	var textarea = $('.code-area textarea');
+	textarea.val(JSON.stringify(attr, null, 4));	
+	var fc = new FusionCharts(JSON.parse(textarea.val())).render();
+	
+});
 
 //element creation and set events
 PROFEEDER.viewAttributes = (function(param){
 	$('.chart-attribute-settings').text('');
-	$.each(PROFEEDER.chartAttributes.responseJSON, function(index_1, val_1) {
+	$.each(PROFEEDER.chartAttributes.returnData.responseJSON, function(index_1, val_1) {
 		if(val_1.table === param)
 		{
 			$.each(val_1.attrs, function(index_2, val_2) {
 				$('.chart-attribute-settings').append(
 					$('<p>').append(function(){
-						$(this).attr('style', 'box-sizing: border-box; display:inline-block; *zoom:1; *display:inline; padding:3px 5px; width:33.3%; margin:0;');
-						$(this).append($('<label style="display:inline-block; min-width:150px;" for="L'+ index_1 + '_' +index_2 +'" title="'+ val_2.description+'">').text(val_2.name));
+						$(this).attr('style', 'box-sizing: border-box; '+
+							'display:inline-block; *zoom:1; *display:inline;'+
+							' padding:3px 5px; width:33.3%; margin:0;');
+						$(this).append($('<label style="display:inline-block;'+
+							' min-width:150px;" for="L'+ index_1 + '_' +index_2 +
+							'" title="'+ val_2.description+'">').text(val_2.name));
 						
 						if(val_2.type === 'Boolean')
 							$(this).append('<input type="checkbox" id="L'+ index_1 + '_' +index_2 +'"/> ');
@@ -126,7 +151,6 @@ PROFEEDER.viewAttributes = (function(param){
 						if(val_2.type === 'String' &&  typeof (val_2.range) === 'undefined')
 							$(this).append('<input type="text" /> ');
 							
-
 					}));
 			});
 		}	
@@ -134,6 +158,7 @@ PROFEEDER.viewAttributes = (function(param){
 
 		//set possible events for each element
 		$('input[type="checkbox"]').on('click' , function(event) {
+			console.log('test');
 			var param ={};
 			param.key = $(this).prev().text();
 	
@@ -152,6 +177,7 @@ PROFEEDER.viewAttributes = (function(param){
 				param.value = $(this).val();
 
 			PROFEEDER.setAttribute(param);
+
 		});
 
 		$('input[type="color"], input[type="number"]').change(function(event) {
@@ -167,11 +193,7 @@ PROFEEDER.viewAttributes = (function(param){
 			param.value = event.target.value;
 			PROFEEDER.setAttribute(param);
 		});
-});
-
-
-
-
+	});
 
 
 })();
