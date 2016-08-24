9bb0fc0d4cb1fb58b15934b65d57e7b78dbee82f
suvradip saha <suvradip@fusioncharts.com>
2016-08-24 15:46:14 +0530

    changes

--- module/assets/js/main.js
+++ module/assets/js/main.js
@@ -36,7 +36,72 @@ PROFEEDER.element = {
 				}
 				temp += '</select>';
 				return temp;
-			}
+			},
+
+	setElements : function(elementObj){
+		
+		$(elementObj.class).empty();
+		
+		$.each(elementObj.attrList, function( _index, _value) {
+			$(elementObj.class).append(
+				$('<p>').append(function(){
+					$(this).attr('style', 'box-sizing: border-box; '+
+						'display:inline-block; *zoom:1; *display:inline;'+
+						' padding:3px 5px; width:33.3%; margin:0;');
+					$(this).append($('<label style="display:inline-block;'+
+						' min-width:150px;" for="L'+ elementObj.index + '_' + _index +
+						'" title="'+ _value.description+'">').text(_value.name));
+					
+					var _setData ;
+					if(typeof elementObj.attrs !== 'undefined')	
+					 _setData = elementObj.attrs.hasOwnProperty(_value.name) ? elementObj.attrs[_value.name] : "";
+
+					if(_value.type === 'Boolean')
+						$(this).append('<input type="checkbox" id="L'+ elementObj.index + '_' + _index +'" '+ 
+							( _setData  === "1" || _value.name.toLowerCase() === 'showlabel' ? 
+								"checked" : "" ) +' /> ');
+					if(_value.type === 'Color')
+						$(this).append('<input type="color" value="'+ ( _setData !== "" ? _setData : '#000000' ) +'" /> ');
+
+					if(_value.type === 'Number')
+					{
+						if(_value.range.indexOf('-') > -1)
+						{
+							var range = _value.range.split('-');						
+							$(this).append(PROFEEDER.element.select(range[0].trim(),
+								range[1].trim())).promise().done(function(){
+								$(this.children('select')).prepend('<option>select</option>');
+								$(this.children('select')).val(( _setData !== "" ?  _setData : "select"));
+							});
+						} else
+							$(this).append('<input type="number" value="'+ ( _setData) +'" /> ');
+					}	
+						
+					if(_value.type === 'String' &&  typeof (_value.range) !== 'undefined'){
+						if(_value.range !== ''){
+							var rng = _value.range.split(',');
+							$(this).append(PROFEEDER.element.select(0,rng.length,rng)).promise().done(function(){
+								
+								$(this.children('select')).prepend('<option>select</option>');
+								$(this.children('select')).val(( _setData !== "" ?  _setData + 1 : "select"));
+
+							});
+						}
+
+						if(_value.range === '')
+							$(this).append('<input type="text" value="'+ ( _setData ) +'" /> ');
+					}
+
+					if(_value.type === 'String' &&  typeof (_value.range) === 'undefined')
+						$(this).append('<input type="text" value="'+ ( _setData ) +'" /> ');
+
+					
+				}));
+			});
+
+
+	PROFEEDER.setEventOnElemennts();
+	}		
 };
 
 
@@ -103,6 +168,13 @@ PROFEEDER.chartData = (function (){
 });
 
 
+PROFEEDER.readTextArea = (function(path){
+	if(path)
+	return JSON.parse($('.code-area textarea').val()).dataSource[path];
+	else
+	return JSON.parse($('.code-area textarea').val());	
+});
+
 //on changes in textArea
 $('.code-area textarea').change(function(event) {
 	for (var charts in FusionCharts.items)
@@ -119,31 +191,32 @@ PROFEEDER.setAttribute = (function(paramData){
 	//temporary fix
 	delete attr.dataSource.setData;
 
+	var opt = ['showlabel', 'showvalues'];
 	//remove the disable attributes
-	if(paramData.value === 'select' || paramData.value === 0 || paramData.value === ''){
-		
-		delete attr.dataSource.chart[paramData.key];
-		
-		//for data object operation
-		if(paramData && PROFEEDER.path === 'data' && 
+	if((paramData.value === 'select' || paramData.value === 0 || paramData.value === '') && 
+		opt.indexOf(paramData.key.toLowerCase()) <= -1){
+
+		//for chart operation
+		if(paramData && !PROFEEDER.path)
+		delete attr.dataSource.chart[paramData.key];	
+
+		//for data operation
+		else if(paramData && PROFEEDER.path === 'data' && 
 			PROFEEDER.level !== 'select' )
 			delete attr.dataSource.data[PROFEEDER.level][paramData.key];
 
+		//for data-level operation
 		else if(paramData && PROFEEDER.path === 'data' && 
 			PROFEEDER.level === 'select')
 			delete attr.dataSource.chart[paramData.key];
 
-		else if(paramData && PROFEEDER.path === 'categories' &&
-			PROFEEDER.level === 'select')
-			delete attr.dataSource.categories[0][paramData.key];
-
-		else if(paramData && PROFEEDER.path === 'categories' &&
+		//for category-level operation
+		else if(paramData && PROFEEDER.path === 'category' &&
 			PROFEEDER.level !== 'select')
 			delete attr.dataSource.categories[0].category[PROFEEDER.level][paramData.key] ;
 		
 		//for trendline operation
-		 if(attr.dataSource.hasOwnProperty('trendlines') && paramData && 
-			PROFEEDER.path === 'line' ){
+		else if(paramData && PROFEEDER.path === 'line' ){
 			delete attr.dataSource.trendlines[0].line[0][paramData.key];
 		}
 
@@ -186,6 +259,12 @@ PROFEEDER.setAttribute = (function(paramData){
 		PROFEEDER.path === 'categories')
 		attr.dataSource.categories[0][paramData.key] = paramData.value;
 
+	//include attribute in category
+	if(attr.dataSource.hasOwnProperty('categories') && paramData && 
+		PROFEEDER.path === 'category'  && PROFEEDER.level !== 'select' )
+		attr.dataSource.categories[0].category[PROFEEDER.level][paramData.key] = paramData.value;
+
+
 	PROFEEDER.chartData.returnData.responseJSON = attr;
 	for (var charts in FusionCharts.items)
         FusionCharts.items[charts].dispose();
@@ -205,90 +284,136 @@ PROFEEDER.viewAttributes = (function(paramObj){
 	param = PROFEEDER.viewAttributes.param;	
 		
 	var temp = PROFEEDER.chartData.returnData.responseJSON.dataSource;	
-	$(paramObj.class).empty();
+	//$(paramObj.class).empty();
 	$.each(PROFEEDER.chartAttributes.returnData.responseJSON, function(index_1, val_1) {
 		if(val_1.table === param)
 		{
-			$.each(val_1.attrs, function(index_2, val_2) {
-			$(paramObj.class).append(
-				$('<p>').append(function(){
-					$(this).attr('style', 'box-sizing: border-box; '+
-						'display:inline-block; *zoom:1; *display:inline;'+
-						' padding:3px 5px; width:33.3%; margin:0;');
-					$(this).append($('<label style="display:inline-block;'+
-						' min-width:150px;" for="L'+ index_1 + '_' +index_2 +
-						'" title="'+ val_2.description+'">').text(val_2.name));
+			// $.each(val_1.attrs, function(index_2, val_2) {
+			// $(paramObj.class).append(
+			// 	$('<p>').append(function(){
+			// 		$(this).attr('style', 'box-sizing: border-box; '+
+			// 			'display:inline-block; *zoom:1; *display:inline;'+
+			// 			' padding:3px 5px; width:33.3%; margin:0;');
+			// 		$(this).append($('<label style="display:inline-block;'+
+			// 			' min-width:150px;" for="L'+ index_1 + '_' +index_2 +
+			// 			'" title="'+ val_2.description+'">').text(val_2.name));
 						
-					temp.setData = temp.chart.hasOwnProperty(val_2.name) ? temp.chart[val_2.name] : "";
+			// 		temp.setData = temp.chart.hasOwnProperty(val_2.name) ? temp.chart[val_2.name] : "";
 
-					if(val_2.type === 'Boolean')
-						$(this).append('<input type="checkbox" id="L'+ index_1 + '_' +index_2 +'" '+ 
-							(temp.setData  === "1" ? "checked" : "" ) +' /> ');
-					if(val_2.type === 'Color')
-						$(this).append('<input type="color" value="'+ (temp.setData) +'" /> ');
+			// 		if(val_2.type === 'Boolean')
+			// 			$(this).append('<input type="checkbox" id="L'+ index_1 + '_' +index_2 +'" '+ 
+			// 				(temp.setData  === "1" || val_2.name.toLowerCase() === 'showlabel' ? 
+			// 					"checked" : "" ) +' /> ');
+			// 		if(val_2.type === 'Color')
+			// 			$(this).append('<input type="color" value="'+ (temp.setData) +'" /> ');
 
-					if(val_2.type === 'Number')
-					{
-						if(val_2.range.indexOf('-') > -1)
-						{
-							var range = val_2.range.split('-');						
-							$(this).append(PROFEEDER.element.select(range[0].trim(),
-								range[1].trim())).promise().done(function(){
-								$(this.children('select')).prepend('<option>select</option>');
-								$(this.children('select')).val((temp.setData !== "" ? temp.setData : "select"));
-							});
-						} else
-							$(this).append('<input type="number" value="'+ (temp.setData) +'" /> ');
-					}	
+			// 		if(val_2.type === 'Number')
+			// 		{
+			// 			if(val_2.range.indexOf('-') > -1)
+			// 			{
+			// 				var range = val_2.range.split('-');						
+			// 				$(this).append(PROFEEDER.element.select(range[0].trim(),
+			// 					range[1].trim())).promise().done(function(){
+			// 					$(this.children('select')).prepend('<option>select</option>');
+			// 					$(this.children('select')).val((temp.setData !== "" ? temp.setData : "select"));
+			// 				});
+			// 			} else
+			// 				$(this).append('<input type="number" value="'+ (temp.setData) +'" /> ');
+			// 		}	
 						
-					if(val_2.type === 'String' &&  typeof (val_2.range) !== 'undefined'){
-						if(val_2.range !== ''){
-							var rng = val_2.range.split(',');
-							$(this).append(PROFEEDER.element.select(0,rng.length,rng)).promise().done(function(){
+			// 		if(val_2.type === 'String' &&  typeof (val_2.range) !== 'undefined'){
+			// 			if(val_2.range !== ''){
+			// 				var rng = val_2.range.split(',');
+			// 				$(this).append(PROFEEDER.element.select(0,rng.length,rng)).promise().done(function(){
 								
-								$(this.children('select')).prepend('<option>select</option>');
-								$(this.children('select')).val((temp.setData !== "" ? temp.setData : "select"));
+			// 					$(this.children('select')).prepend('<option>select</option>');
+			// 					$(this.children('select')).val((temp.setData !== "" ? temp.setData : "select"));
 
-							});
-						}
+			// 				});
+			// 			}
 
-						if(val_2.range === '')
-							$(this).append('<input type="text" value="'+ (temp.setData ) +'" /> ');
-					}
+			// 			if(val_2.range === '')
+			// 				$(this).append('<input type="text" value="'+ (temp.setData ) +'" /> ');
+			// 		}
 
-					if(val_2.type === 'String' &&  typeof (val_2.range) === 'undefined')
-						$(this).append('<input type="text" value="'+ (temp.setData ) +'" /> ');
+			// 		if(val_2.type === 'String' &&  typeof (val_2.range) === 'undefined')
+			// 			$(this).append('<input type="text" value="'+ (temp.setData ) +'" /> ');
 
 					
-				}));
-			});
+			// 	}));
+			// });
+
+			// PROFEEDER.element.setElements({ 'attrList': val_1.attrs,
+			// 								'index': index_1,
+			// 								'attrs': temp.chart,
+			// 								'class': paramObj.class });
 			
-			if(val_1.path && paramObj.next && val_1.path ==='data'){
-				PROFEEDER.path = val_1.path;
-				$('.chart-attribute-settings').prepend(
+			if(!val_1.path){
+			PROFEEDER.element.setElements({ 'attrList': val_1.attrs,
+											'index': index_1,
+											'attrs': temp.chart,
+											'class': paramObj.class });
+
+			} else if(val_1.path && val_1.path !=='categories' ){
+
+			var config ={};
+			PROFEEDER.path = val_1.path;
+
+			if(val_1.path ==='data'){	
+			PROFEEDER.element.setElements({ 'attrList': val_1.attrs,
+			 								'index': index_1,
+			 								'attrs': temp.data,
+			 								'class': paramObj.class });
+			config.length = temp.data.length;
+			}
+
+			if(val_1.path ==='category'){	
+			PROFEEDER.element.setElements({ 'attrList': val_1.attrs,
+			 								'index': index_1,
+			 								'attrs': temp.categories[0].category,
+			 								'class': paramObj.class });	
+			config.length = temp.categories[0].category.length;
+			}
+							
+			if(val_1.path ==='line'){	
+			PROFEEDER.element.setElements({ 'attrList': val_1.attrs,
+			 								'index': index_1,
+			 								'attrs': (temp.hasOwnProperty('trendlines') ? 
+			 											temp.trendlines[0].line :'undefined'),
+			 								'class': paramObj.class });
+			config.length = temp.hasOwnProperty('trendlines') ? temp.trendlines[0].line.length : 0;
+			} 									
+
+			
+			$('.chart-attribute-settings').prepend(
 					$('<div class="data-attr">').append(function(){
 						$(this).append(
 							$('<p>').append(function(){
 								$(this).text('select ' + val_1.path + ' level' );
-								$(this).append(PROFEEDER.element.select(1, temp.data.length));
-								 $(this).children('select').prepend('<option>select</option>');
-								 $(this).children('select').val("select");
+								$(this).append(PROFEEDER.element.select(1, config.length));
+								$(this).children('select').prepend('<option>select</option>');
+								$(this).children('select').val("select");
 								$(this).children('select').off();
 								//default depth
 								PROFEEDER.level = 'select';
 								//if level change
 								$(this).children('select').on('click', function(event) {
 									PROFEEDER.level = event.target.value;
-								
+
 								});
 
 
 							}));				
 					}));
 			
-			} else if(val_1.path && paramObj.next && val_1.path ==='categories'){
+			} else if(val_1.path ==='categories'){
 				PROFEEDER.path = val_1.path;
-			} 
+				PROFEEDER.element.setElements({ 'attrList': val_1.attrs,
+			 								'index': index_1,
+			 								'attrs': temp.categories[0],
+			 								'class': paramObj.class });	
+
+			}
 
 
 
@@ -296,7 +421,7 @@ PROFEEDER.viewAttributes = (function(paramObj){
 	});
 
 		//set possible events for each element
-		PROFEEDER.setEventOnElemennts();
+		//PROFEEDER.setEventOnElemennts();
 
 		//set last selected sidebar cosmetic
 		PROFEEDER.viewAttributes.param = param;	
@@ -341,6 +466,26 @@ PROFEEDER.setEventOnElemennts = (function(){
 			PROFEEDER.setAttribute(param);
 		});
 });
+
+ PROFEEDER.takeOverConsole = function(){
+    var original = window.console;
+    window.console = {
+        log: function(){
+            // do sneaky stuff
+            arguments = "hello";
+            original.log.apply(original, arguments);
+        },
+        warn: function(){
+            // do sneaky stuff
+            original.warn.apply(original, arguments);
+        },
+        error: function(){
+            // do sneaky stuff
+            original.error.apply(original, arguments);
+        }
+    };
+};
+
 
 window.PROFEEDER = PROFEEDER;
 })();
